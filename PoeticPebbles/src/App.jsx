7ff0a5c89dc083f354b15bc09ae44beafc736412
Amankaushik2810 <amankaushik2810@gmail.com>
2024-06-23 16:22:40 +0530

    Create Pages and Routes

--- PoeticPebbles/src/App.jsx
+++ PoeticPebbles/src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import { BrowserRouter } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/Aboutt'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Project from './pages/Project'
 const App = () => {
   return (
     <div>
-      <h1 className='taxt-5xl text-red-400'>Hello</h1>
+      <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>}/>
+      <Route path="/sign-in" element={<SignIn/>}/>
+      <Route path="/sign-up" element={<SignUp/>}/>
+      <Route path="/Dashboard" element={<Dashboard/>}/>
+      <Route path="/Project" element={<Project/>}/>
+
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
